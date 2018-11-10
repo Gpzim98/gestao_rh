14c505fe9140c5a09752b1edcdf50b197f31e1d2
Gregory Pacheco <gpzim98@gmail.com>
2018-11-10 15:02:14 +0000

    Aula17 - Crud Empresa parte 2, vinculando funcionario

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic.edit import CreateView
 from .models import Empresa
 
@@ -6,3 +6,10 @@ from .models import Empresa
 class EmpresaCreate(CreateView):
     model = Empresa
     fields = ['nome']
+
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.empresa = obj
+        funcionario.save()
+        return HttpResponse('Ok')
--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 from apps.departamentos.models import Departamento
 from apps.empresas.models import Empresa
 
