edb601becbae257197078e00566b7df76d4952be
Gregory Pacheco <gpzim98@gmail.com>
2018-11-10 19:40:55 +0000

    Crud Funcionario, Create

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import FuncionariosList, FuncionarioEdit, FuncionarioDelete
+from .views import (
+    FuncionariosList,
+    FuncionarioEdit,
+    FuncionarioDelete,
+    FuncionarioNovo
+)
 
 urlpatterns = [
     path('', FuncionariosList.as_view(), name='list_funcionarios'),
+    path('novo/', FuncionarioNovo.as_view(), name='create_funcionario'),
     path('editar/<int:pk>/', FuncionarioEdit.as_view(), name='update_funcionario'),
     path('delete/<int:pk>/', FuncionarioDelete.as_view(), name='delete_funcionario'),
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
-from django.views.generic import ListView, UpdateView, DeleteView
+from django.views.generic import (
+    ListView,
+    UpdateView,
+    DeleteView,
+    CreateView
+)
+
 from .models import Funcionario
 
 
@@ -19,3 +26,16 @@ class FuncionarioEdit(UpdateView):
 class FuncionarioDelete(DeleteView):
     model = Funcionario
     success_url = reverse_lazy('list_funcionarios')
+
+
+class FuncionarioNovo(CreateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
+
+    def form_valid(self, form):
+        funcionario = form.save(commit=False)
+        username = funcionario.nome.split(' ')[0] + funcionario.nome.split(' ')[1]
+        funcionario.empresa = self.request.user.funcionario.empresa
+        funcionario.user = User.objects.create(username=username)
+        funcionario.save()
+        return super(FuncionarioNovo, self).form_valid(form)
