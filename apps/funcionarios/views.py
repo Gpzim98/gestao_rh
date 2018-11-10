c839630b52390072f8f3c27f8890a3596ea93659
Gregory Pacheco <gpzim98@gmail.com>
2018-11-10 18:42:47 +0000

    aula 20 - Crud Funcionarios, filtrando pela empresa logada

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -4,3 +4,7 @@ from .models import Funcionario
 
 class FuncionariosList(ListView):
     model = Funcionario
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Funcionario.objects.filter(empresa=empresa_logada)
