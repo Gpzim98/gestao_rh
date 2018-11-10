f3e2a388ad903dc80e81674c2c2996ce98d3eb1e
Gregory Pacheco <gpzim98@gmail.com>
2018-11-10 20:07:48 +0000

    aula 26 - Crud Departamentos, Update

--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView
+)
 from .models import Departamento
 
 
@@ -19,3 +23,8 @@ class DepartamentoCreate(CreateView):
         departamento.empresa = self.request.user.funcionario.empresa
         departamento.save()
         return super(DepartamentoCreate, self).form_valid(form)
+
+
+class DepartamentoUpdate(UpdateView):
+    model = Departamento
+    fields = ['nome']
