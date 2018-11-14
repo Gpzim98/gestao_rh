948774e4863fd5b48d44afb8691a2be7d5910968
Gregory Pacheco <gpzim98@gmail.com>
2018-11-14 23:27:51 +0000

    Adicionando .gitignore e requirements

--- apps/departamentos/urls.py
+++ apps/departamentos/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import (
     DepartamentosList,
     DepartamentoCreate,
-    DepartamentoUpdate
+    DepartamentoUpdate,
+    DepartamentoDelete
 )
 
 urlpatterns = [
     path('list', DepartamentosList.as_view(), name='list_departamentos'),
     path('novo', DepartamentoCreate.as_view(), name='create_departamento'),
     path('update/<int:pk>/', DepartamentoUpdate.as_view(), name='update_departamento'),
+    path('delete/<int:pk>/', DepartamentoDelete.as_view(), name='delete_departamento'),
 ]
--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,7 +1,9 @@
+from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     CreateView,
-    UpdateView
+    UpdateView,
+    DeleteView
 )
 from .models import Departamento
 
@@ -28,3 +30,8 @@ class DepartamentoCreate(CreateView):
 class DepartamentoUpdate(UpdateView):
     model = Departamento
     fields = ['nome']
+
+
+class DepartamentoDelete(DeleteView):
+    model = Departamento
+    success_url = reverse_lazy('list_departamentos')
