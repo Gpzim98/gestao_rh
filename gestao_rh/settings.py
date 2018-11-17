d75896bead4b50f67046085c3a39cc8ebc0a713d
Gregory Pacheco <gpzim98@gmail.com>
2018-11-17 11:38:38 +0000

    Romovendo pasta de arquivos estaticos do root

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -88,7 +88,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
