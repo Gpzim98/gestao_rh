aaa221175a7a11fe77716c81e4b0cb7281984933
Gregory Pacheco <gpzim98@gmail.com>
2018-12-25 19:59:28 +0000

    Aula 70 Lendo e escrevendo em banco de dados antigos

--- apps/app_antiga/admin.py
+++ apps/app_antiga/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Teste
+from .models import Teste, RegistroUsuarios
+
+
+class RegistroUsuarioAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return RegistroUsuarios.objects.using('antigo').all()
 
 
 admin.site.register(Teste)
+admin.site.register(RegistroUsuarios, RegistroUsuarioAdmin)
--- apps/app_antiga/models.py
+++ apps/app_antiga/models.py
@@ -4,3 +4,13 @@ from django.db import models
 class Teste(models.Model):
     descricao = models.TextField()
 
+
+class RegistroUsuarios(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    salario = models.DecimalField(decimal_places=2, max_digits=7)
+
+    class Meta:
+        db_table = 'registro_usuarios'
+
