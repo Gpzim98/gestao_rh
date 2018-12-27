0d6f185028466f19be9aed7317262b0fb4217744
Gregory Pacheco <gpzim98@gmail.com>
2018-12-27 22:50:14 +0000

    Aula 90 - Resolvendo problema na chamada ajax

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
 
 from apps.funcionarios.api.views import FuncionarioViewSet
 from apps.registro_hora_extra.api.views import RegistroHoraExtraViewSet
@@ -18,11 +17,6 @@ router.register(r'api/banco-horas', RegistroHoraExtraViewSet)
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-
-urlpatterns += i18n_patterns(
     path('', include('apps.core.urls')),
     path('funcionarios/', include('apps.funcionarios.urls')),
     path('departamentos/', include('apps.departamentos.urls')),
@@ -30,7 +24,8 @@ urlpatterns += i18n_patterns(
     path('documento/', include('apps.documentos.urls')),
     path('horas-extras/', include('apps.registro_hora_extra.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
 
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
