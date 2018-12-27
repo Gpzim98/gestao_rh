7dc72840bedd3bdaf95afac5e97f9a0ddbe612e9
Gregory Pacheco <gpzim98@gmail.com>
2018-12-27 19:53:44 +0000

    Sistema de traducoes do Django

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -1,4 +1,5 @@
 import os
+from django.utils.translation import ugettext_lazy as _
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -56,6 +58,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
             ],
         },
     },
@@ -78,7 +81,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
@@ -113,5 +116,15 @@ CELERY_TASK_SERIALIZER = 'json'
 
 
 DATABASE_ROUTERS = ['gestao_rh.DBRoutes.DBRoutes']
+
+LANGUAGES = (
+    ('en', _('English')),
+    ('pt', _('Portugues')),
+    ('es', _('Spanish')),
+)
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
 
 from .local_settings import *
--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 from apps.funcionarios.api.views import FuncionarioViewSet
 from apps.registro_hora_extra.api.views import RegistroHoraExtraViewSet
@@ -17,6 +18,11 @@ router.register(r'api/banco-horas', RegistroHoraExtraViewSet)
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+
+urlpatterns += i18n_patterns(
     path('', include('apps.core.urls')),
     path('funcionarios/', include('apps.funcionarios.urls')),
     path('departamentos/', include('apps.departamentos.urls')),
@@ -28,4 +34,4 @@ urlpatterns = [
 
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
